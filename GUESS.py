46f21a9eee7b0345f7d43124cdd7f36c5eb95493
adarsh-kumar-14-12-05 <103896889+adarsh-kumar-14-12-05@users.noreply.github.com>
2022-04-17 23:19:34 +0530

    Add files via upload

---
+++ GUESS.py
@@ -0,0 +1,72 @@
+print("Welcone to my computer quiz!")
+print("COPYRIGHT @BLUE BIRD")
+
+playing = input("Do you want to play the game : \n")
+
+if playing.upper()!="YES":
+    quit()
+
+print("Okay Lets Get Started :)\n")
+
+score = 0
+
+#Q1
+answer = input("What does CPU stands for- \n")
+
+if answer.lower() == "central processing unit":
+    score+=1
+
+    print("Correct ! Lets go ")
+
+else:
+    print("Incorrect answer => correct answer is central processing unit !")
+
+#Q2
+answer = input("What does ALU stands for - \n")
+
+if answer.lower() == "arithematic and logical unit":
+     score+=1
+
+     print("Correct ! Let's go ")
+
+else:
+    print("Incorrect answer => correct answer is arithematic and logical unit ! ")
+
+#Q3
+answer = input("What does GPU stands for - \n")
+
+if answer.lower() == "graphics processing unit":
+    score+=1
+
+    print("Correct ! Lets go ")
+
+else:
+    print("Incorrect answer => correct answer is graphics processing unit !")
+
+#Q4
+answer = input("What does CU stands for - \n")
+
+if answer.lower() == "control unit":
+    score+=1
+
+    print("Correct ! Lets go ")
+
+else:
+    print("Incorrect answer => correct answer is control unit !")
+
+#Q5
+answer = input("What does RAM stands for - \n")
+
+if answer.lower() == "random access memory":
+    score+=1
+
+    print("Correct ! Lets go ")
+
+else:
+    print("Incorrect answer => correct answer is random access memory !")
+
+print(f"Good job ! You got {score} questions correct !\n")
+
+print(f"Your score was {(score/5)*100}% \n")
+
+print("Make sure to improve the code by adding more question to it or minimising the lines of code because that's what an actual programmer needs I guess !\n")
